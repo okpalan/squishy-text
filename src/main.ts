fae33c80dbc36e8b08ed9b1ba7be727981ee53ab
Nnamdi Michael Okpala <okpalan86@gmail.com>
2023-05-06 18:32:39 +0100

    second-first commit

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 const canvas = document.getElementById("sqishy-text") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
 
-
+const textOptions = {
+    data: "Squeeze Me",
+}
